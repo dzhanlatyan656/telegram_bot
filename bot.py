9e62460afbbd7ce989c1c9f734812396f0bcf1db
Daniel Dzhanlatian <dzhanlatyan@gmail.com>
2018-05-17 15:40:20 +0300

    Новая версия

--- bot.py
+++ bot.py
@@ -3,8 +3,6 @@ from telegram.ext import Updater, CommandHandler
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
     
-def greet_user(bot, update):
-    print('Вызван /start')
 
 def main():
     mybot = Updater("509144200:AAGoB1WgyWiasouuaO2jPhD7IG0pHRz2iyc", request_kwargs=PROXY)
@@ -14,5 +12,9 @@ def main():
 
     mybot.start_polling()
     mybot.idle()
+
+
+def greet_user(bot, update):
+    print('Вызван /start')
 
 main()
