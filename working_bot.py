865a5f307b745d0fe9d1771efa6d8bf3c613c939
Daniel Dzhanlatian <dzhanlatyan@gmail.com>
2018-05-20 22:45:31 +0300

    up to date

--- working_bot.py
+++ working_bot.py
@@ -1,5 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+import ephem
+
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
@@ -10,12 +12,12 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     )
 
 def main():
-    mybot = Updater("TelegramKey", request_kwargs=PROXY)
+    mybot = Updater("509144200:AAGoB1WgyWiasouuaO2jPhD7IG0pHRz2iyc", request_kwargs=PROXY)
     
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-
+    dp.add_handler(CommandHandler("planet",choose_planet,pass_args=True))
 
     mybot.start_polling()
     mybot.idle()
@@ -29,8 +31,9 @@ def greet_user (bot, update):
 def talk_to_me(bot, update):
     user_text = update.message.text
     print(user_text)
-    update.message.reply_text(':********')
+    update.message.reply_text(user_text)
 
-
+def choose_planet(bot, update, args):
+    pass
 
 main()
